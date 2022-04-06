8b8769c7e0e5b0aa90179df4bf4f2fdcec49bf7b
jacktembo <jack@jacktembo.com>
2022-04-06 18:21:02 +0000

    cores

--- buses/admin.py
+++ buses/admin.py
@@ -13,22 +13,7 @@ class BusCompanyImageInline(admin.StackedInline):
 
 @admin.register(Bus)
 class BusAdmin(admin.ModelAdmin):
-    def save_model(self, request, obj, form, change):
-        obj.bus_admin = request.user
-        super().save_model(request, obj, form, change)
-
-    def get_queryset(self, request):
-        """
-        Allowing Events admins to only access only those events that were created by them.
-        """
-        qs = super(BusAdmin, self).get_queryset(request)
-        if request.user.is_superuser:
-            return qs
-        return qs.filter(bus_admin=request.user)
-
-    search_fields = ['bus_company', 'bus_short_name', ]
-    list_filter = ['bus_company']
-    list_display = ['bus_company', 'bus_full_name', 'number_of_seats', 'mobile_money_number', ]
+    list_display = ['bus_company', 'bus_full_name', 'number_of_seats', ]
     inlines = [BusIMageInline]
 
 
@@ -40,7 +25,6 @@ class BusCompanyAdmin(admin.ModelAdmin):
     inlines = [BusCompanyImageInline, ]
 
 
-@admin.register(Ticket)
 class TicketAdmin(admin.ModelAdmin):
     list_display = [
         'ticket_number', 'passenger_first_name', 'passenger_last_name', 'date_bought'
@@ -48,14 +32,10 @@ class TicketAdmin(admin.ModelAdmin):
     search_fields = ['passenger_first_name']
     list_per_page = 5
 
-
-@admin.register(Route)
 class RouteAdmin(admin.ModelAdmin):
-    def departure_time(self, route: Route):
-        return route.time.strftime('%H:%M hrs')
-
     list_display = [
         'bus', 'starting_place', 'destination', 'departure_time', 'price'
     ]
-
-
+    def departure_time(self)
+admin.site.register(Route, RouteAdmin)
+admin.site.register(Ticket, TicketAdmin)
