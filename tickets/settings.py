0650e18c6c50f3333373e6ae68c9c7a75952bcd1
jacktembo <jack@jacktembo>
2022-04-29 15:53:47 +0200

    Have rendered bus companies on homepage.

--- tickets/settings.py
+++ tickets/settings.py
@@ -136,7 +136,7 @@ prod_db = {
         },
     }
 }
-DATABASES = dev_db
+DATABASES = prod_db
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
