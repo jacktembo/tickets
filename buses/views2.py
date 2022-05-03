f895236dbab8e08af0199d2766d8cf0441572b53
jacktembo <jack@jacktembo>
2022-05-03 15:21:24 +0200

    form b

--- buses/views2.py
+++ buses/views2.py
@@ -9,3 +9,12 @@ def index(request):
         'bus_companies': bus_companies
     }
     return render(request, 'index.html', context)
+
+
+def bus_company_detail(request, pk):
+    bus_company = BusCompany.objects.get(pk=pk)
+    routes = Route.objects.filter(bus__bus_company=bus_company)
+    context = {
+        'bus_company': bus_company, 'routes': routes
+    }
+    return render(request, 'bus_detail.html', context)
